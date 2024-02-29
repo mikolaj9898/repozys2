1fa56baa5b5b31f7a6000c194aa1fdc575f63f7e
mikolaj9898 <113548703+mikolaj9898@users.noreply.github.com>
2024-02-29 21:14:53 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function z35() {
+    window.open("z35_Kraj.html", "okienko_z35","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z36() {
+    window.open("z36_Kraj.html", "okienko_z36","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z38() {
+    window.open("z38_Kraj.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z38_w() {
+    window.open("z38w_Kraj.html", "okienko_z382","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z39() {
+    window.open("z39_Kraj.html", "okienko_z39","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ function z40() {
+    window.open("z40_Kraj.html", "okienko_z40_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+ }
