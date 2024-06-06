39b583ac0964ea9d98fa0c8f8eb7dfb0a2a8c19a
mikolaj9898 <113548703+mikolaj9898@users.noreply.github.com>
2024-06-06 22:30:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,88 @@ function z35() {
  }
  function z40() {
     window.open("z40_Kraj.html", "okienko_z40_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+
  }
+function z41() {
+   window.open("z41_kraj.html", "okienko_z41w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z42() {
+   window.open("z42_kraj.html", "okienko_z42w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z43() {
+   window.open("z43_kraj.html", "okienko_z43w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z44() {
+   window.open("z44_kraj.html", "okienko_z44w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z45() {
+   window.open("z45_kraj.html", "okienko_z45w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z46() {
+   window.open("z46_kraj.html", "okienko_z46w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z47() {
+   window.open("z47_kraj.html", "okienko_z47w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z50w() {
+   window.open("z50_kraj.html", "okienko_z50w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z51() {
+   window.open("z51_kraj.html", "okienko_z51w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z52() {
+   window.open("z52_kraj.html", "okienko_z52w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z53() {
+   window.open("z53_kraj.html", "okienko_z53w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z56w() {
+   window.open("z56_kraj.html", "okienko_z56w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z57() {
+   window.open("z57_kraj.html", "okienko_z57w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z58() {
+   window.open("z58_kraj.html", "okienko_z58w","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z42_kod() {
+   window.open("z42_kraj_kod.html", "okienko_z42w_kod","toolbar=no, directories=no, menubar=no, height=720, width=900, top=350, left=600");
+}
+
+function z43_kod() {
+   window.open("z43_kraj_kod.html", "okienko_z43w_kod","toolbar=no, directories=no, menubar=no, height=720, width=900, top=350, left=600");
+}
+
+function z44_kod() {
+   window.open("z44_kraj_kod.html", "okienko_z44w_kod","toolbar=no, directories=no, menubar=no, height=720, width=900, top=350, left=600");
+}
+
+function z45_kod() {
+   window.open("z45_kraj_kod.html", "okienko_z45w_kod","toolbar=no, directories=no, menubar=no, height=720, width=900, top=350, left=600");
+}
+
+function z46_kod() {
+   window.open("z46_kraj_kod.html", "okienko_z46w_kod","toolbar=no, directories=no, menubar=no, height=720, width=900, top=350, left=600");
+}
+
+function z47_kod() {
+   window.open("z47_kraj_kod.html", "okienko_z47w_kod","toolbar=no, directories=no, menubar=no, height=720, width=900, top=350, left=600");
+}
+
+function z50_kod() {
+   window.open("z50_kraj_kod.html", "okienko_z50w_kod","toolbar=no, directories=no, menubar=no, height=720, width=900, top=350, left=600");
+}
